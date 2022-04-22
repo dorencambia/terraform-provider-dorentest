b3cbaaa25e68521f7266aa42c3789a4ae66870d9
Doren <doren.proctor@cambiahealth.com>
2022-04-21 18:34:50 -0700

    fix bugs: set -e, git tag --points-at, $zip_file_names

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
-ls
+set -e # exit if any line fails
 
-version=`git tag`
+version=`git tag --points-at`
 version=${version#v} # remove the v at the beginning
 provider_name="dorentest"
 tf_org="cambia-demo"
@@ -11,7 +11,7 @@ zip_file_names="darwin_amd64 darwin_arm64 freebsd_386 freebsd_amd64 freebsd_arm 
 download_release_files() {
     base_download_url="https://github.com/dorencambia/terraform-provider-${provider_name}/releases/download"
     files="SHA256SUMS SHA256SUMS.sig"
-    for zip_file in zip_file_names; do
+    for zip_file in $zip_file_names; do
         files+=" $zip_file.zip"
     done
     for file in $files; do
